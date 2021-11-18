1038daf01e810e844d44ddd04a9227f4a821e251
Starl <Starl@DESKTOP-BUK7FPK>
2021-11-18 15:49:09 -0400

    Arreglo de herencia

--- Proyecto Final/src/Logico/Jurado.java
+++ Proyecto Final/src/Logico/Jurado.java
@@ -7,13 +7,46 @@ public class Jurado extends Personas {
 	private String recomendacion;
 	private String lugarDeRecomendacion;
 
-	public Jurado(String nombre, String direccion, String cedula, String apellido, String telefono, String correo) {
+	public Jurado(String nombre, String direccion, String cedula, String apellido, String telefono, String correo, 
+			String areaDeConocimiento, int añoDeExperiencia, String recomendacion, String lugarDeRecomendacion) {
 		super(nombre, direccion, cedula, apellido, telefono, correo);
 		
 	
 		this.areaDeConocimiento = areaDeConocimiento;
 		this.añoDeExperiencia = añoDeExperiencia;
 		this.recomendacion = recomendacion;
+		this.lugarDeRecomendacion = lugarDeRecomendacion;
+	}
+
+	public String getAreaDeConocimiento() {
+		return areaDeConocimiento;
+	}
+
+	public void setAreaDeConocimiento(String areaDeConocimiento) {
+		this.areaDeConocimiento = areaDeConocimiento;
+	}
+
+	public int getAñoDeExperiencia() {
+		return añoDeExperiencia;
+	}
+
+	public void setAñoDeExperiencia(int añoDeExperiencia) {
+		this.añoDeExperiencia = añoDeExperiencia;
+	}
+
+	public String getRecomendacion() {
+		return recomendacion;
+	}
+
+	public void setRecomendacion(String recomendacion) {
+		this.recomendacion = recomendacion;
+	}
+
+	public String getLugarDeRecomendacion() {
+		return lugarDeRecomendacion;
+	}
+
+	public void setLugarDeRecomendacion(String lugarDeRecomendacion) {
 		this.lugarDeRecomendacion = lugarDeRecomendacion;
 	}
 
--- Proyecto Final/src/Logico/Participantes.java
+++ Proyecto Final/src/Logico/Participantes.java
@@ -4,36 +4,35 @@ import java.util.ArrayList;
 
 public class Participantes extends Personas {
 	
-	private String matricula;
-	private String proyecto;
+	private String Matricula;
+	private String Proyecto;
 	private String institucionProveniente;
 	private ArrayList<Trabajos> misTrabajos;
 
 	public Participantes(String nombre, String direccion, String cedula, String apellido, String telefono,
-			String correo) {
+			String correo, String matricula, String proyecto, String institucionesProvenientes) {
 		super(nombre, direccion, cedula, apellido, telefono, correo);
-		this.matricula = matricula;
-		this.proyecto = proyecto;
-		this.institucionProveniente = institucionProveniente;
+		this.Matricula = matricula;
+		this.Proyecto = proyecto;
+		this.institucionProveniente = institucionesProvenientes;
 		misTrabajos = new ArrayList<>();
 		
 	}
 
-	
 	public String getMatricula() {
-		return matricula;
+		return Matricula;
 	}
 
 	public void setMatricula(String matricula) {
-		this.matricula = matricula;
+		Matricula = matricula;
 	}
 
 	public String getProyecto() {
-		return proyecto;
+		return Proyecto;
 	}
 
 	public void setProyecto(String proyecto) {
-		this.proyecto = proyecto;
+		Proyecto = proyecto;
 	}
 
 	public String getInstitucionProveniente() {
@@ -53,5 +52,4 @@ public class Participantes extends Personas {
 	}
 
 	
-
 }
